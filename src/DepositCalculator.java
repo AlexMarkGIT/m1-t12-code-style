5016d9486b8278ae61d3993ddfbe7a14fb3c5251
Егор <egich-2011@mail.ru>
2023-05-23 17:37:40 +0300

    Есть, что доработать)

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -10,27 +10,27 @@ public class DepositCalculator {
         return round(amount + amount * yearRate * depositPeriod, 2);
     }
 
-    double round(double value, int places) {
-        double scaLe = Math.pow(10, places);
+    double round(double value, int places) { // Здесь лучше переименовать метод с использованием глагола get.
+        double scaLe = Math.pow(10, places); // Здесь лучше назвать переменную scale, а то нет таких слов sca Le :)
         return Math.round(value * scaLe) / scaLe;
     }
 
-    void calculateDeposit(){
+    void calculateDeposit(){ // Здесь лучше переименовать метод с использованием глагола set.
         int amount;
         int period;
         int action;
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // Здесь по code style лучше отделить через enter
         System.out.println("Введите сумму вклада в рублях:");
-        amount = scanner.nextInt();
+        amount = scanner.nextInt(); // Здесь отделить
         System.out.println("Введите срок вклада в годах:");
-        period = scanner.nextInt();
+        period = scanner.nextInt();// Здесь отделить
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
 
         double outAmount = 0;
 
-        if (action == 1) outAmount = calculateSimplePercent(amount, 0.06, period);
+        if (action == 1) outAmount = calculateSimplePercent(amount, 0.06, period); // Здесь лучше использовать формат if(){}
         else if (action == 2) {
             outAmount = calculateComplexPercent(amount, 0.06, period);
         }
